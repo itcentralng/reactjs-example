5a2e51ded9c072ff4db22e37a532661e3838baf6
Nasir Mustapha <teboy.com@gmail.com>
2023-09-01 16:26:35 +0100

    react example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
+import logo2 from './logo2.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [currentLogo, setCurrentLogo] = useState('logo1')
+
+  const [count, setCount] = useState(1)
+
+  const logos = {logo1:logo, logo2:logo2}
+
+  const Logo = ()=>{
+      return <img src={logos[currentLogo]} className="App-logo" alt="logo" />
+  }
+
+  const HeaderBody = (props)=>{
+    return <div>
+          <p>
+            {props.text}
+          </p>
+          <button onClick={
+            ()=>{
+              if (currentLogo === 'logo1'){
+                setCurrentLogo('logo2')
+              }else{
+                setCurrentLogo('logo1')
+              }
+            }
+          }>Change Logo</button>
+          </div>
+  }
+
+  const Counter = ()=>{
+    return <div>
+      <p>{count}</p>
+      <button onClick={
+        ()=>{
+          setCount(count+1)
+        }
+      }>+</button>
+      <button onClick={
+        ()=>{
+          setCount(count-1)
+        }
+      }>-</button>
+    </div>
+  }
+
+  const Header = (props)=>{
+    return <header className="App-header">
+            <Logo logo={props.logo}/>
+            {/* <HeaderBody text='This is a test'/> */}
+            <HeaderBody text='This is a pretty one'/>
+            <Counter/>
+          </header>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header logo={logo2}/>
+      {/* <Header logo={logo}/> */}
     </div>
   );
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+let people = [1, 2, 3, 4, 5, 6, 7, 8];
+
+people.map(number=>console.log(number))
+console.log(people.filter(number=> number % 2 == 0))
